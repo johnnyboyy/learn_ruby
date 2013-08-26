5d5bb06280ce7080b10ba8e5a05301ef956985b5
johnnyboyy <john.zdanis@gmail.com>
2013-08-26 16:37:23 -0400

    fixed RPN Calculator

--- bonus_rpn_calculator/rpn_calculator.rb
+++ bonus_rpn_calculator/rpn_calculator.rb
@@ -7,7 +7,7 @@ class RPNCalculator
 	end
 
 	def value
-		@value = @stack.pop
+		@value = @stack[-1]
 	end
 
 	def push(num)
@@ -17,14 +17,76 @@ class RPNCalculator
 	def plus
 		if @stack.length < 1
 			raise "calculator is empty"
-		elsif @stack.length == 1
-			p @stack
-			num = @stack.pop
-			@value += num
 		else
 			num_two, num_one = @stack.pop, @stack.pop
 			@stack << (num_one + num_two)
 		end
 	end
+
+	def minus
+		if @stack.length < 1
+			raise "calculator is empty"
+		else
+			num_two, num_one = @stack.pop, @stack.pop
+			@stack << (num_one - num_two)
+		end
+	end
+
+
+	def times
+		if @stack.length < 1
+			raise "calculator is empty"
+		else
+			num_two, num_one = @stack.pop, @stack.pop
+			@stack << (num_one * num_two)
+		end
+	end
+
+	def divide
+		if @stack.length < 1
+			raise "calculator is empty"
+		else
+			num_two, num_one = @stack.pop.to_f, @stack.pop.to_f
+			@stack << num_one / num_two
+		end
+	end
+
+	def tokens(str)
+		tokenized = str.split()
+
+
+		tokenized.map! do |i|
+			if /\d/ === i
+				i.to_i
+			else
+				i.to_sym
+			end				
+		end
+	end
+
+
+	def evaluate(str)
+		@value = 0.0
+		tokenized_arr = tokens(str)
+
+		tokenized_arr.each do |t|
+			if t.class == 1.class
+				@stack << t
+			elsif t == :+
+				self.plus
+			elsif t == :-
+				self.minus
+			elsif t == :*
+				self.times
+			elsif t == :/
+				self.divide
+			else
+				raise "Error: Bad Token #{t}"
+			end
+		end
+		return self.value
+	end
+
+
 
 end
